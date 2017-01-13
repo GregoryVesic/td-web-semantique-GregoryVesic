2e51902653b4f67ef36f422ce80499254f4a7bac
GregoryVesic <gregory.vesic@etu.unice.fr>
2017-01-13 18:07:36 +0100

    [NodeJS] Inscription fonctionnelle !

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -16,11 +16,11 @@ app.set('views', __dirname + '/views');
 
 /* On affiche le formulaire d'enregistrement */
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.redirect('/login');
 });
 
-app.get('/login', function(req, res){
+app.get('/login', function (req, res) {
     res.render('login');
     res.redirect('/register');
 });
@@ -28,10 +28,10 @@ app.get('/login', function(req, res){
 app.post('/login', function (req, res) {
     var username = req.body.username;
     var mdp = req.body.password;
-    verif(username,mdp);
+    verif(username, mdp);
 });
 
-app.get('/inscription', function(req, res){
+app.get('/inscription', function (req, res) {
     res.render('register');
 });
 
@@ -48,19 +48,21 @@ app.get('/profile', function (req, res) {
 
 app.post('/req_inscription', function (req, res) {
     var email = req.body.email;
-    var mdp = req.body.mdp2;
+    //var mdp = req.body.mdp2;
+    var mdp = req.body.password;
     var nom = req.body.nom;
     var prenom = req.body.prenom;
     var tel = req.body.tel;
-    var url = req.body.url;
+    var url = req.body.website;
     var sexe = req.body.sexe;
     var birthdate = req.body.birthdate;
-    var age = req.body.age;
+    //var age = req.body.age;
     var ville = req.body.ville;
     var taille = req.body.taille;
     var couleur = req.body.couleur;
     var profilepic = req.body.profilepic;
-    inserer(email, mdp, nom, prenom, tel, url, sexe, birthdate, ville, taille, birthdate, profilepic);
+    logger.info(email, mdp, nom, prenom, tel, url, sexe, birthdate, ville, taille, couleur, profilepic);
+    inserer(req.body);
 });
 
 logger.info('server start');
@@ -76,36 +78,40 @@ var connection = mysql.createConnection({
     port: 3307
 });
 
-function verif(username,mdp){
+function verif(username, mdp) {
     connection.connect();
 
-    connection.query("select email from users where email='" + username + "' AND password='"+mdp+"'",function(err,rows, fields){
-    //connection.query("select * from users",function(err,rows, fields){
+    connection.query("select email from users where email='" + username + "' AND password='" + mdp + "'", function (err, rows, fields) {
+        //connection.query("select * from users",function(err,rows, fields){
         //logger.info("select email from users where email='" + username + "' AND password='"+mdp+"'");
-        if(!err) {
+        if (!err) {
             if (rows.length > 0) {
                 logger.info('Authentification valide !');
             }
             else {
                 logger.info('Authentification non valide !');
             }
-        }
-        else{
-            logger.info('Erreur SQL !');
-        }
-    });
-    connection.end();
-}
 
-function inserer(email, mdp, nom, prenom, tel, url, sexe, birthdate, ville, taille, birthdate, profilepic) {
-    connection.connect();
-    connection.query("INSERT INTO users (email, password, nom, prenom, website, sexe, birthdate, ville, taille, profilepic) "
-        + "VALUES ('" + email + "','" + "'test123'" + "','" + nom + "','" + prenom + "','" + tel + "','" + url + "','" + sexe + "'," + birthdate + ",'" + ville + "'," + taille + ",'" + profilepic + "')", function(err, result){
-        if (!err) {
-            logger.info('Insertion : OK');
         }
         else {
             logger.info('Erreur SQL !');
+        }
+        connection.end();
+    });
+
+}
+
+// userInfo : contenu des champs du body dans un JSON
+function inserer(userInfo) {
+    //connection.connect();
+    connection.query("INSERT INTO users (email, password, nom, prenom, tel, website, sexe, birthdate, ville, taille, couleur, profilepic) VALUES ('" + userInfo.email + "','" + userInfo.mdp + "','" + userInfo.nom + "','" + userInfo.prenom + "','" + userInfo.tel + "','" + userInfo.website + "','" + userInfo.sexe + "', '" + userInfo.birthdate + "','" + userInfo.ville + "', " + userInfo.taille + ",'" + userInfo.couleur + "','" + userInfo.profilepic + "')", function (err, result) {
+        logger.debug(result);
+        if (!err) {
+           logger.info('Insertion : OK');
+        } else {
+           logger.info('Erreur SQL !');
+           connection.close();
+           throw err;
         }
     });
 }
